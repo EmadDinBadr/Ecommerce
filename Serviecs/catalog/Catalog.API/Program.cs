5fc8abe9bd5108f9e33cf015c903b023de38cc74
Emad Din Badr Din <emadin9910@outlook.com>
2025-12-01 18:49:41 +0200

    first commit

---
+++ Serviecs/catalog/Catalog.API/Program.cs
@@ -0,0 +1,29 @@
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
