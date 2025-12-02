be0b9d7fc34d8dd909091beaece6ea78768df8fa
Emad Din Badr Din <emadin9910@outlook.com>
2025-12-02 16:41:55 +0200

    .

---
+++ Serviecs/catalog/Catalog.Core/Entities/ProductBrand.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Catalog.Core.Entities
+{
+    public class ProductBrand: BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
