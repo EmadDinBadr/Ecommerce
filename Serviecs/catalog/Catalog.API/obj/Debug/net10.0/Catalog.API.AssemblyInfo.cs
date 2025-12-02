ae047fdec43479e37227384b67316b534c1cc132
Emad Din Badr Din <emadin9910@outlook.com>
2025-12-02 16:41:36 +0200

    add entities

--- Serviecs/catalog/Catalog.API/obj/Debug/net10.0/Catalog.API.AssemblyInfo.cs
+++ Serviecs/catalog/Catalog.API/obj/Debug/net10.0/Catalog.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc8abe9bd5108f9e33cf015c903b023de38cc74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Serviecs/catalog/Catalog.Core/obj/Debug/net10.0/Catalog.Core.AssemblyInfo.cs
+++ Serviecs/catalog/Catalog.Core/obj/Debug/net10.0/Catalog.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc8abe9bd5108f9e33cf015c903b023de38cc74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
